2baa2e6d7b8c6e7404eeacd8cdc9fa40fa945c4f
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2023-07-01 15:44:02 -0500

    ✨ add nuxt frontend

---
+++ frontend/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <header class="w-full h-16 bg-rose-600 text-white">
+        <nav class="w-full h-full px-4 flex items-center">
+            nav
+        </nav>
+    </header>
+    <div class="w-full h-96 flex items-center justify-center">
+            <div class="w-full h-full flex items-center justify-center">
+            <h4 class="text-rose-600 md:text-2xl">Bloody</h4>
+        </div>
+    </div>
+</template>
